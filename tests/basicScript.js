6c2dc5673974c7a72619fb5e4082fea64b5593ff
Duncan Gossage <eyeamaman@gmail.com>
2016-10-31 13:41:43 +0000

    First attempt at getting Travis to work

---
+++ tests/basicScript.js
@@ -0,0 +1,13 @@
+
+
+describe('makeBasicScript', function() {
+
+  Yo.init();
+  Yo.add('hello', function() {
+    return "Hello!";
+  });
+
+  it('Should return "Hello!"', function() {
+    expect(Yo.modules.hello).toBe('Hello!');
+  });
+});
