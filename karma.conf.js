942e8b308b9a2e46aaf7bcc9347845a6c8b3ff0e
= <eyeamaman@gmail.com>
2017-05-27 15:20:30 -0700

    Slight change to see if the scripts are running

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'dev/yo.js ', 'tests/**/*.js'
+      'dev/yo.js',
+      'tests/**/*.js'
     ],
 
 
@@ -55,7 +56,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
